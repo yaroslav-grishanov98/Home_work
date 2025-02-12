d3a60c54dba44d4b3bca05f18c423e3d22020f54
Yaroslav <yaroslav.grishanov.98@bk.ru>
2025-02-13 00:22:12 +0300

    Окончательный вариает работы, после проверки и добавления изменений

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,27 @@
-def mask_account_card(input_string):
-    # Применение маскировки в зависимости от типа
+def mask_account_card(input_string: str) -> str:
     if "Счет" in input_string:
-        account_number = input_string.split(" ", 1)[1]  # Получаем только номер счета
-        masked_number = f"**{account_number[-4:]}"  # Маскируем все кроме последних 4 цифр
+        account_number = input_string.split(" ", 1)[1]
+        masked_number = f"**{account_number[-4:]}"
         return f"Счет {masked_number}"
     else:
-        card_info = input_string.split(" ", 1)  # Разделяем на название карты и номер
+        card_info = input_string.split(" ", 1)
         card_type = card_info[0]
-        card_number = card_info[1]
+        card_number = card_info[1].replace(' ', '')
 
-        # Применение маскировки для карт
-        if len(card_number) == 16:
-            masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"  # Маскируем карту
+
+        if len(card_number) >= 16:
+            masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
         else:
-            # Обработка карт с другим количеством цифр
-            masked_number = card_number[:-4].replace(card_number[:-4], '**') + card_number[-4:]
+            masked_number = f"{'*' * (len(card_number) - 4)}{card_number[-4:]}"
 
         return f"{card_type} {masked_number}"
 
+def get_mask_card_number(card_input: str) -> str:
+    return mask_account_card(card_input)
 
-# Примеры использования
-print(mask_account_card("Visa Platinum 7000792289606361"))  # Visa Platinum 7000 79** **** 6361
-print(mask_account_card("Счет 73654108430135874305"))  # Счет **4305
+def get_mask_account(account_input: str) -> str:
+    return mask_account_card(account_input)
+
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
