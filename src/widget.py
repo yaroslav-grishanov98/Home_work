8e76d853b95673c56ad87ef7c083294e2eab8f97
Yaroslav <yaroslav.grishanov.98@bk.ru>
2025-03-04 18:28:12 +0300

    Внес изменения в виджет файл поскольку не проходила функция в тесте

--- src/widget.py
+++ src/widget.py
@@ -4,16 +4,22 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(numbers: Any) -> Any:
-    """Создаем новую функцию, которая также макскирует номер, но теперь с названием счета (карты)"""
+    """Mask account or card number based on input type."""
     new_numbers = numbers.split(" ")
+
     if "Счет" in numbers:
-        return f"Счет {get_mask_account(numbers)}"
+        masked_account = get_mask_account(new_numbers[-1])
+        return f"Счет {masked_account}" if masked_account else "Счет "
     else:
-        return f"{' '.join(new_numbers[0:-1])} {get_mask_card_number(new_numbers[-1])}"
+        masked_card = get_mask_card_number(new_numbers[-1])
+        return masked_card if masked_card else "Карта "
 
 
 def get_date(date: Union[str]) -> Union[str, int]:
-    """Создаем функцию, которая возвращает дату в ДД.ММ.ГГГГ"""
+    """Функция, которая возвращает дату в формате ДД.ММ.ГГГГ."""
+
+    if not date:  # Проверка на пустую строку
+        return ""
 
     new_data = date.split("T")
     correct_data = new_data[0].split("-")
